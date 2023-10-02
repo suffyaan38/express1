f1bc3c9870e5261e64c97065ef9044ea0fd0c8fb
suffyaan38 <mohammadsuffyaankhan123@gmail.com>
2023-10-02 16:40:17 +0530

    add product and routes handling

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const http=require('http');
 const express=require('express');
+const bodyParser = require('body-parser');
 
 const app=express();
-
-app.use((req,res,next)=>{
-    console.log("in the middleware");
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity"> <input type="submit" value="add product"> </form>')
 });
-app.use((req,res,next)=>{
-    console.log("in the next middleware");
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello from Express</h1>')
 });
 
